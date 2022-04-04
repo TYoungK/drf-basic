802218ebfae2edadaf704ac9eb7f9f4ed28df84f
TYoungK <xordud525@nate.com>
2022-04-04 20:01:20 +0900

    add APIView

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,50 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from .serializers import *
 from .models import *
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
 
+
+# class PostListCreateAPIView(APIView):
+#
+#     def get(self, request):
+#         articles = Post.objects.filter(active=True)
+#         serializer = PostSerializer(articles, many=True)
+#         # queryset many=True, Model instance면 없어도 됨.
+#         return Response(serializer.data)
+#
+#     def post(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class PostDetailAPIView(APIView):
+#
+#     def get_object(self, pk):
+#         article = get_object_or_404(Post, pk=pk)
+#         return article
+#
+#     def get(self, request, pk):
+#         article = self.get_object(pk)
+#         serializer = PostSerializer(article)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk):
+#         article = self.get_object(pk)
+#         serializer = PostSerializer(article, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk):
+#         article = self.get_object(pk)
+#         article.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class PostView(viewsets.ModelViewSet):
     queryset = Post.objects.all()
